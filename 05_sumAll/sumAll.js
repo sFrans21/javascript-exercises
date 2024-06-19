7177cd8d5dc27777d0fefb3dcf4e09fa80d8b32a
sFrans21 <118550565+sFrans21@users.noreply.github.com>
2024-06-19 12:45:23 +0700

    number 5 submitted

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function (number1, number2) {
+  if (!Number.isInteger(number1) || !Number.isInteger(number2)) {
+    return "ERROR";
+  }
+  if (number1 < 0 || number2 < 0) {
+    return "ERROR";
+  }
+  let sumOfAll = 0;
+  let selisih = number2 - number1;
+  if (selisih < 0) {
+    sumOfAll = number2;
+    selisih += (0 - selisih) * 2;
+    for (let i = 0; i < selisih; i++) {
+      sumOfAll += number1 - i;
+    }
+  } else {
+    sumOfAll = number1;
+    for (let i = 0; i < selisih; i++) {
+      sumOfAll += number2 - i;
+    }
+  }
+  return sumOfAll;
 };
 
 // Do not edit below this line
