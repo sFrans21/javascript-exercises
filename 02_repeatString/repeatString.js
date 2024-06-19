e1072fbfa061793fba35aa0b06d2a8585061fb82
sFrans21 <118550565+sFrans21@users.noreply.github.com>
2024-06-19 11:39:53 +0700

    number 2 submitted

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function (string, number) {
+  let basketOfString = "";
+  if (number == -1) {
+    return "ERROR";
+  } else if (string == "") {
+    return "";
+  }
+  for (let i = 0; i < number; i++) {
+    basketOfString += string;
+  }
+  return basketOfString;
 };
 
 // Do not edit below this line
