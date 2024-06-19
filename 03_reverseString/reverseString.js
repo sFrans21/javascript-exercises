99d3a20988978f2c1c0cf554e76677305bd81d0d
sFrans21 <118550565+sFrans21@users.noreply.github.com>
2024-06-19 11:55:08 +0700

    number 3 submitted

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseStrings = function (strings) {
+  let reversedStrings = "";
+  for (let i = 1; i <= strings.length; i++) {
+    let partitionOfStrings = strings[strings.length - i];
+    reversedStrings += partitionOfStrings;
+  }
+  return reversedStrings;
 };
 
 // Do not edit below this line
-module.exports = reverseString;
+module.exports = reverseStrings;
