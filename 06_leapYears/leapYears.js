76968b44c176b2a5c7936234548f24e3d2312f72
sFrans21 <118550565+sFrans21@users.noreply.github.com>
2024-06-19 17:39:49 +0700

    submitted no 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function (number) {
+  if (number % 100 != 0) {
+    return number % 4 == 0;
+  } else {
+    return number % 400 == 0;
+  }
 };
-
 // Do not edit below this line
 module.exports = leapYears;
