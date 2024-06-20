d8b44ef50cc249b5ec78c85efa859936a2c478eb
sFrans21 <118550565+sFrans21@users.noreply.github.com>
2024-06-20 10:21:50 +0700

    submitted no 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (number) {
+  if (number >= 0) {
+    return parseFloat(((number - 32) * (5 / 9)).toFixed(1));
+  } else {
+    return parseFloat(((number - 32) * (5 / 9)).toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (number) {
+  if (number >= 0) {
+    return parseFloat((number * (9 / 5) + 32).toFixed(1));
+  } else {
+    return parseFloat((number * (9 / 5) + 32).toFixed(1));
+  }
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
